71144e25e7de2040f5e20c5336616db8d51a68ff
vanh2000-git <nguyenvietanh102000@gmail.com>
2024-11-15 16:26:14 +0700

    sửa

--- tudien.js
+++ tudien.js
@@ -1,10 +1,11 @@
 let a = ["banana", "apple", "orange"];
 let b = ["chuối", "táo", "cam"];
 let value = prompt('Nhập vào một từ');
-for (let i = 0; i < length; i++) {
+let index = -1;
+for (let i = 0; i < a.length; i++) {
     if(a[i]===value){
-        alert(b[i]);
+        index=i;
         break;
     }
 }
-
+index>=0?alert(`${b[index]}`):alert('từ không có trong mảng');
